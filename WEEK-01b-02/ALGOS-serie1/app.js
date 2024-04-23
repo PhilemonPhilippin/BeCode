b3e446dd3819fc60e6febeed4ff70df441d4246d
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-23 10:19:38 +0200

    Exercices 5 et 6 finis

--- WEEK-01b-02/ALGOS-serie1/app.js
+++ WEEK-01b-02/ALGOS-serie1/app.js
@@ -76,13 +76,57 @@ console.log("=============================================");
 //#endregion
 
 //#region 04
-console.log("Exercice 04: ");
-console.log("===============");
-function detectWord(str) {
-    return str.replace(/[A-Z]/g, '');
-}
-console.log(detectWord("UcUNFYGaFYFYGtNUH"));
-console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
-console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
+// console.log("Exercice 04: ");
+// console.log("===============");
+// function detectWord(str) {
+//     return str.replace(/[A-Z]/g, '');
+// }
+// console.log(detectWord("UcUNFYGaFYFYGtNUH"));
+// console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+// console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
 //#endregion
 
+//#region 05
+// console.log("Exercice 05: ");
+// console.log("===============");
+// function sortDrinkByPrice(drinks) {
+//     return drinks.sort(compareByPrice);
+// }
+// function compareByPrice(a, b) {
+//     if (a['price'] < b['price']) {
+//         return -1;
+//     }
+//     else if (a['price'] > b['price']) {
+//         return 1;
+//     }
+//     else {
+//         return 0;
+//     }
+// }
+// console.log(
+//     sortDrinkByPrice([
+//     { name: "water", price: 3 },
+//     { name: "coke", price: 8 },
+//     { name: "lemonade", price: 10 },
+//     { name: "lime", price: 10 },
+//     { name: "vodka", price: 17 },
+//     ])
+// );
+//#endregion
+
+//#region 06
+// console.log("Exercice 06: ");
+// console.log("===============");
+// function animals(chickens, cows, pigs) {
+//     return chickens * 2 + (cows + pigs) * 4;
+// }
+// console.log(animals(2, 3, 5));
+// console.log(animals(1, 2, 3));
+// console.log(animals(5, 2 ,8));
+//#endregion
+
+//#region 07
+// console.log("Exercice 07: ");
+// console.log("===============");
+
+//#endregion
