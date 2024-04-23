c93e7dc82e0bae76fdcdc4082b162d55246ae795
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-23 09:42:04 +0200

    Exercices algo 00 a 02 finis

--- WEEK-01b-02/ALGOS-serie1/app.js
+++ WEEK-01b-02/ALGOS-serie1/app.js
@@ -1,1 +1,66 @@
-console.log("Hello");
+console.log("Hello World! Welcome in ALGOS-serie1 folder.");
+console.log("=============================================");
+//#region 00
+// console.log("Exercice 00: ");
+// console.log("===============");
+// function makesTen(x, y) {
+//     if (Number.isInteger(x) && Number.isInteger(y)) {
+//         return x === 10 || y === 10 || x + y === 10;
+//     }
+//     else {
+//         return null;
+//     }
+// }
+// console.log(makesTen(9, 10));
+// console.log(makesTen(9, 9));
+// console.log(makesTen(1, 9));
+//#endregion
+
+//#region 01
+// console.log("Exercice 01: ");
+// console.log("===============");
+// function calcAge(ageInYears) {
+//     if (Number.isInteger(ageInYears) && ageInYears >= 0) {
+//         return ageInYears * 365;
+//     }
+//     else {
+//         return null;
+//     }
+// }
+// console.log(calcAge(65));
+// console.log(calcAge(0));
+// console.log(calcAge(20));
+//#endregion
+
+//#region 02
+// console.log("Exercice 02: ");
+// console.log("===============");
+// function addUp(x) {
+//     let retval;
+//     if (Number.isInteger(x) && x >= 1 && x <= 1000) {
+//         if (x % 2 === 0) {
+//             retval = x + (x - 1) * (x / 2);
+//         }
+//         else {
+//             retval = x + x * Math.floor(x / 2);
+//         }
+//         // Trouver l'algorithme optimal
+//         // x = 2 && retval = 2 + 1 * 1 == 3
+//         // x = 3 && retval = 3 + 3 * 1 == 6 === x % 2 !== 0 DONC x + x * INT(x/2)
+//         // x = 4 && retval = 4 + 3 * 2 == 10 === x % 2 === 0 DONC x + (x-1) * (x/2)
+//         // x = 5 && retval = 5 + 5 * 2 == 15 === x % 2 !== 0 DONC x + x * INT(x/2)
+//         // x = 6 && retval = 6 + 5 * 3 == 21 === x % 2 === 0 DONC x + (x-1) * (x/2)
+//         // x = 7 && retval = 7 + 7 * 3 == 28 etc.
+//         // x = 8 && retval = 8 + 7 * 4 == 36 etc.
+//         // x = 9 && retval = 9 + 9 * 4 == 45 etc.
+//     }
+//     else {
+//         retval = null;
+//     }
+//     return retval;
+// }
+// console.log(addUp(4));
+// console.log(addUp(13));
+// console.log(addUp(600));
+//#endregion
+
