b2f345629a827200c6a8b2b11f26bbb2538ab7fd
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-24 16:49:52 +0200

    Algos series 2 exercices 0 à 3 finis

--- WEEK-03-04/02.ALGOS-serie2/app.js
+++ WEEK-03-04/02.ALGOS-serie2/app.js
@@ -1,1 +1,58 @@
-console.log("Hello World");
+console.log("Hello World! Welcome in 02.ALGOS folder.");
+console.log("=============================================");
+//#region 00
+// console.log("Exercice 00: ");
+// console.log("===============");
+// function countTrue(arr) {
+//     if (arr.length === 0) {
+//         return 0;
+//     }
+//     return arr.reduce((previous, current) => previous = current === true ? previous + 1 : previous, 0);
+// }
+// console.log(countTrue([true, false, false, true, false]));
+// console.log(countTrue([false, false, false, false]));
+// console.log(countTrue([]));
+//#endregion
+
+//#region 01
+// console.log("Exercice 01: ");
+// console.log("===============");
+// const maxRoll = 6;
+// function possibleBonus(myPosition, friendPosition) {
+//     if (myPosition >= friendPosition) {
+//         return false;
+//     }
+//     return myPosition + maxRoll >= friendPosition;
+// }
+// console.log(possibleBonus(3, 7));
+// console.log(possibleBonus(1, 9));
+// console.log(possibleBonus(5, 3));
+//#endregion
+
+//#region 02
+// console.log("Exercice 02: ");
+// console.log("===============");
+// function num_of_digits(number) {
+//     return number.toString().length;
+// }
+// console.log(num_of_digits(1000));
+// console.log(num_of_digits(12));
+// console.log(num_of_digits(1305981031));
+// console.log(num_of_digits(0));
+//#endregion
+
+//#region 03
+// console.log("Exercice 03: ");
+// console.log("===============");
+// function toArray(objectToConvert) {
+//     let retval = [];
+//     for (const key in objectToConvert) {
+//         retval.push([key, objectToConvert[key]]);
+//     }
+//     return retval;
+// }
+// console.log(toArray({ a: 1, b: 2 }));
+// console.log(toArray({ shrimp: 15, tots: 12 }));
+// console.log(toArray({}));
+//#endregion
+
