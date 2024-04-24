dac2047981bfbc689fa4e8c12cb9c11fe59f28bb
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-24 15:50:45 +0200

    Structure faite pour les algos Week 3 et 4

--- Coding-Challenge/app.js
+++ Coding-Challenge/app.js
@@ -1,1 +1,7 @@
-console.log("test");
+function getMiddle(s) {
+  const sLength = s.length;
+  return sLength % 2 === 0 ? s[sLength / 2 - 1] + s[sLength / 2] : s[Math.floor(s.length / 2)];
+}
+
+console.log(getMiddle("testing"));
+console.log(getMiddle("middle"));
