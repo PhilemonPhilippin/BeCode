d64ea07aa19c42e28f06f58fb6292176b145bf41
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-25 14:57:56 +0200

    Algos serie 3 exercices 3 à 6 finis

--- WEEK-03-04/03.ALGOS-serie3/app.js
+++ WEEK-03-04/03.ALGOS-serie3/app.js
@@ -65,4 +65,72 @@ console.log("=============================================");
 // console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length"));
 //#endregion
 
+//#region 03
+// console.log("Exercice 03: ");
+// console.log("===============");
+// function isSmooth(str) {
+//     let smooth = true;
+//     let arr = str.split(' ');
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         if (arr[i].slice(-1).toLowerCase() !== arr[i+1].slice(0, 1).toLowerCase()) {
+//             smooth = false;
+//         }
+//     }
+//     return smooth;
+// }
+// console.log(isSmooth("Marta appreciated deep perpendicular right trapezoids"));
+// console.log(isSmooth("Someone is outside the doorway"));
+// console.log(isSmooth("She eats super righteously"));
+//#endregion
 
+//#region 04
+// console.log("Exercice 04: ");
+// console.log("===============");
+// function sevenBoom(arr) {
+//     return arr.join('').includes('7') ? "Boom!" : "there is no 7 in the array";
+// }
+// console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]));
+// console.log(sevenBoom([8, 6, 33, 100]));
+// console.log(sevenBoom([2, 55, 60, 97, 86]));
+//#endregion
+
+//#region 05
+// console.log("Exercice 05: ");
+// console.log("===============");
+// function convert(temperature) {
+//     if (temperature.slice(-1) === 'C') {
+//         let fahrenheit = Math.round(parseFloat(temperature.substring(0, temperature.length - 1)) * 9 / 5 + 32);
+//         return fahrenheit.toString() + '°F';
+//     }
+//     else if (temperature.slice(-1) === 'F') {
+//         let celsius = Math.round((parseFloat(temperature.substring(0, temperature.length - 1)) - 32) * 5 / 9);
+//         return celsius.toString() + '°C';
+//     }
+//     else {
+//         return "Error";
+//     }
+// }
+// console.log(convert("35°C"));
+// console.log(convert("19°F"));
+// console.log(convert("33"));
+//#endregion
+
+//#region 06
+// console.log("Exercice 06: ");
+// console.log("===============");
+// function findBrokenKeys(expected, given) {
+//     let retval = new Set();
+//     let strLength = expected.length;
+//     let expectedArr = expected.split('');
+//     let givenArr = given.split('');
+//     for (let i = 0; i < strLength; i++) {
+//         if (expectedArr[i] !== givenArr[i]) {
+//             retval.add(expectedArr[i]);
+//         }
+//     }
+//     return Array.from(retval);
+// }
+// console.log(findBrokenKeys("happy birthday", "hawwy birthday"));
+// console.log(findBrokenKeys("starry night", "starrq light"));
+// console.log(findBrokenKeys("beethoven", "affthoif5"));
+//#endregion
