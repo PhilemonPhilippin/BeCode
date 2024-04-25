23ba8aad005c889f2c29d27fc8ed86c344ac1c86
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-25 16:26:21 +0200

    Exercice 04.ALGOS-advanced 00 fini

--- WEEK-03-04/04.ALGOS-advanced/app.js
+++ WEEK-03-04/04.ALGOS-advanced/app.js
@@ -1,7 +1,28 @@
 console.log("Hello World! Welcome in 04.ALGOS-ADVANCED folder.");
 console.log("=============================================");
 //#region 00
+// console.log("Exercice 00: ");
+// console.log("===============");
+// function fizzBuzz(number) {
+//     for (let i = 1; i <= number; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         }
+//         else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         }
+//         else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         }
+//         else {
+//             console.log(i);
+//         }
+//     }
+// }
+// fizzBuzz(20);
+//#endregion
+
+//#region 01
 console.log("Exercice 00: ");
 console.log("===============");
-
 //#endregion
