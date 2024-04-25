d1907d1cc14735f4251013475080b31370763c38
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-25 15:03:52 +0200

    Skeleton done for 04.ALGOS-advanced

---
+++ WEEK-03-04/04.ALGOS-advanced/app.js
@@ -0,0 +1,7 @@
+console.log("Hello World! Welcome in 04.ALGOS-ADVANCED folder.");
+console.log("=============================================");
+//#region 00
+console.log("Exercice 00: ");
+console.log("===============");
+
+//#endregion
