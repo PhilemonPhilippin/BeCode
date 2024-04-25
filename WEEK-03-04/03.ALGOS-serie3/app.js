d9d9554d01c941f8fd484663d1ac8c0250ccfb37
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-25 12:16:44 +0200

    Algos series 3 exercices 0 à 2 finis

---
+++ WEEK-03-04/03.ALGOS-serie3/app.js
@@ -0,0 +1,68 @@
+console.log("Hello World! Welcome in 03.ALGOS-serie3 folder.");
+console.log("=============================================");
+//#region 00
+// console.log("Exercice 00: ");
+// console.log("===============");
+// function oddishOrEvenish(number) {
+//     let str = number.toString();
+//     let sum = Array.from(str).reduce((previous, current) => previous += parseInt(current), 0);
+//     return sum % 2 === 0 ? "Evenish" : "Oddish";
+// }
+// console.log(oddishOrEvenish(43));
+// console.log(oddishOrEvenish(373));
+// console.log(oddishOrEvenish(4433));
+//#endregion
+
+//#region 01
+// console.log("Exercice 01: ");
+// console.log("===============");
+// function getTotalPrice(groceries) {
+//     let totalPrice = 0;
+//     for (const grocery of groceries) {
+//         totalPrice += grocery['price'] * grocery['quantity'];
+//     }
+//     return totalPrice.toFixed(2);
+// }   
+// console.log(getTotalPrice([{ product: "Milk", quantity: 1, price: 1.5 }]));
+// console.log(
+//   getTotalPrice([
+//     { product: "Milk", quantity: 1, price: 1.5 },
+//     { product: "Cereals", quantity: 1, price: 2.5 },
+//   ])
+// );
+// console.log(getTotalPrice([{ product: "Milk", quantity: 3, price: 1.5 }]));
+// console.log(
+//   getTotalPrice([
+//     { product: "Milk", quantity: 1, price: 1.5 },
+//     { product: "Eggs", quantity: 12, price: 0.1 },
+//     { product: "Bread", quantity: 2, price: 1.6 },
+//     { product: "Cheese", quantity: 1, price: 4.5 },
+//   ])
+// );
+// console.log(
+//   getTotalPrice([
+//     { product: "Chocolate", quantity: 1, price: 0.1 },
+//     { product: "Lollipop", quantity: 1, price: 0.2 },
+//   ])
+// );
+//#endregion
+
+//#region 02
+// console.log("Exercice 02: ");
+// console.log("===============");
+// function reverseOdd(str) {
+//     let retval = '';
+//     let arr = str.split(' ');
+//     for (const word of arr) {
+//         retval += word.length % 2 !== 0 ? word.split('').reverse().join('') : word;
+//         retval += ' ';
+//     }
+//     retval.trim();
+//     return retval;
+// }
+// console.log(reverseOdd("Bananas"));
+// console.log(reverseOdd("One two three four"));
+// console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length"));
+//#endregion
+
+
