c5652230db33eec770a523e7488461434912f546
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-25 10:15:58 +0200

    Algos series 2 exercices 4 à 6 finis

--- WEEK-03-04/02.ALGOS-serie2/app.js
+++ WEEK-03-04/02.ALGOS-serie2/app.js
@@ -1,4 +1,4 @@
-console.log("Hello World! Welcome in 02.ALGOS folder.");
+console.log("Hello World! Welcome in 02.ALGOS-serie2 folder.");
 console.log("=============================================");
 //#region 00
 // console.log("Exercice 00: ");
@@ -56,3 +56,62 @@ console.log("=============================================");
 // console.log(toArray({}));
 //#endregion
 
+//#region 04
+// console.log("Exercice 04: ");
+// console.log("===============");
+// function arrayOfMultiples(num, length) {
+//     let retval = [num];
+//     let multiplicator = 2;
+//     for (let i = 1; i < length; i++) {
+//         retval.push(num * multiplicator);
+//         multiplicator++;
+//     }
+//     return retval;
+// }
+// console.log(arrayOfMultiples(7, 5));
+// console.log(arrayOfMultiples(12, 10));
+// console.log(arrayOfMultiples(17, 6));
+//#endregion
+
+//#region 05
+// console.log("Exercice 05: ");
+// console.log("===============");
+// function removeLeadingTrailing(n) {
+//     return parseFloat(n).toString();
+// }
+// console.log(removeLeadingTrailing("230.000"));
+// console.log(removeLeadingTrailing("00402"));
+// console.log(removeLeadingTrailing("03.1400"));
+// console.log(removeLeadingTrailing("30"));
+//#endregion
+
+//#region 06
+// console.log("Exercice 06: ");
+// console.log("===============");
+// function sortIt(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             let firstElement = Array.isArray(arr[i]) ? arr[i][0] : arr[i];
+//             let secondElement = Array.isArray(arr[j]) ? arr[j][0] : arr[j];
+//             if (secondElement < firstElement) {
+//                 const temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(sortIt([4, 1, 3]));
+// console.log(sortIt([[4], [1], [3]]));
+// console.log(sortIt([4, [1], 3]));
+// console.log(sortIt([[4], 1, [3]]));
+// console.log(sortIt([[3], 4, [2], [5], 1, 6]));
+//#endregion
+
+//#region 07
+// console.log("Exercice 07: ");
+// console.log("===============");
+
+//#endregion
