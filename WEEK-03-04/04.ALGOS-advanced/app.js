bd2f57f909a6064226ae4dd39c116d02824c7a02
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-26 10:26:26 +0200

    Exercice 04.ALGOS-advanced 01 fini

--- WEEK-03-04/04.ALGOS-advanced/app.js
+++ WEEK-03-04/04.ALGOS-advanced/app.js
@@ -23,6 +23,60 @@ console.log("=============================================");
 //#endregion
 
 //#region 01
-console.log("Exercice 00: ");
-console.log("===============");
+// console.log("Exercice 01: ");
+// console.log("===============");
+// function ransomNote(noteText, magazineText) {
+//     let retval = true;
+//     let magazineArray = magazineText.split(' ');
+//     let noteArray = noteText.split(' ');
+//     let noteArrayWithCount = convertArrayToArrayWithCount(noteArray);
+//     let magazineArrayWithCount = convertArrayToArrayWithCount(magazineArray);
+//     for (let i = 0; i < noteArrayWithCount.length; i++) {
+//         let isWordIncludedWithAdequatCount = false;
+//         for (let j = 0; j < magazineArrayWithCount.length; j++) {
+//             if (
+//             noteArrayWithCount[i]["word"] ===
+//                 magazineArrayWithCount[j]["word"] &&
+//             noteArrayWithCount[i]["count"] <= magazineArrayWithCount[j]["count"]
+//             ) {
+//             isWordIncludedWithAdequatCount = true;
+//             }
+//         }
+//         if (isWordIncludedWithAdequatCount === false) {
+//             retval = false;
+//         }
+//     }
+//     return retval;
+// }
+
+// function convertArrayToArrayWithCount(arr) {
+//     let countingArray = [];
+//     for (let i = 0; i < arr.length; i++) {
+//     if (countingArray.some((obj) => obj["word"] === arr[i]) === false) {
+//         countingArray.push({ word: arr[i], count: 1 });
+//     } 
+//     else {
+//         let indexOfDuplicate = countingArray.findIndex((x) => x["word"] === arr[i]);
+//         countingArray[indexOfDuplicate]["count"] += 1;
+//     }
+//     }
+//     return countingArray;
+// }
+// // Test cases :
+// //===========================
+// console.log(ransomNote(
+//   "this is a secret note to you from a secret admirer",
+//   "puerto rico is a great place you must hike far from town to find a secret waterfall that i am an admirer of but note that it is not as hard as it seems this is my advice to you"
+// ));
+
+// console.log(
+//   ransomNote(
+//     "this is a note to you from a secret admirer",
+//     "puerto rico is a great place you must hike far from town to find a secret waterfall that i am an admirer of but note that it is not as hard as it seems this is my advice to you"
+//   )
+// );
+
+// console.log(ransomNote("my magazine my many words", "my magazine has many words and many letters wow it is my magazine"))
+
+// console.log(ransomNote("my magazine my many words my", "my magazine has many words and many letters wow it is my magazine"));
 //#endregion
