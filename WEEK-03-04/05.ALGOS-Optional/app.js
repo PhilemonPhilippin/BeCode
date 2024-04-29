68205ccc7d7d90194b990a8f49656a50f39576ac
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-29 10:39:03 +0200

    Skeleton done for 05.ALGOS-Optional

---
+++ WEEK-03-04/05.ALGOS-Optional/app.js
@@ -0,0 +1,8 @@
+console.log("Hello World! Welcome in 05.ALGOS-Optional folder.");
+console.log("=============================================");
+//#region 01
+console.log("Exercice 01: ");
+console.log("===============");
+
+
+//#endregion
