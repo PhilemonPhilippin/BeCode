b0a8d971857823cb1f56ef970eefbaea26404c38
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-29 12:27:39 +0200

    05.ALGOS-Optional exercices 1 a 8 finis

--- WEEK-03-04/05.ALGOS-Optional/app.js
+++ WEEK-03-04/05.ALGOS-Optional/app.js
@@ -1,7 +1,82 @@
 console.log("Hello World! Welcome in 05.ALGOS-Optional folder.");
 console.log("=============================================");
 //#region 01
-console.log("Exercice 01: ");
+// console.log("Exercice 01: ");
+// console.log("===============");
+// const removeDuplicates = arr => Array.from(new Set(arr));
+// console.log(removeDuplicates([4, 9, 5, 1, 3, 2, 4, 1, 8]));
+// console.log(removeDuplicates(["hello", "world", "goodbye", "world"]));
+// console.log(removeDuplicates([true, true, false, true, true, false]));
+//#endregion
+
+//#region 02
+// console.log("Exercice 02: ");
+// console.log("===============");
+// const capitalize = str => str[0].toUpperCase() + str.slice(1);
+// console.log(capitalize("belgium"));
+// console.log(capitalize("brazil"));
+// console.log(capitalize("brussels"));
+//#endregion
+
+//#region 03
+// console.log("Exercice 03: ");
+// console.log("===============");
+// const dayDif = (day1, day2) => Math.abs(day1 - day2) / (1000 * 60 * 60 * 24);
+// console.log(dayDif(new Date("2020-10-21"), new Date("2021-10-22")));
+//#endregion
+
+//#region 04
+// console.log("Exercice 04: ");
+// console.log("===============");
+// const average = (...args) => args.reduce((aggregate, current) => aggregate = aggregate + current, 0) / args.length;
+// console.log(average(1, 2, 3, 4));
+//#endregion
+
+//#region 05
+// console.log("Exercice 05: ");
+// console.log("===============");
+// const getSmallest = arr => Math.min(...arr);
+// console.log(getSmallest([13, 7, 11, 3, 9, 15, 17]));
+//#endregion
+
+//#region 06
+// console.log("Exercice 06: ");
+// console.log("===============");
+// const areEqual = (arr1, arr2) => arr1.every((x) => arr2.includes(x));
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [3, 1, 4, 2];
+// const arr3 = [1, 2, 3];
+// console.log(areEqual(arr1, arr2));
+// console.log(areEqual(arr1, arr3));
+//#endregion
+
+//#region 07
+// console.log("Exercice 07: ");
+// console.log("===============");
+// const randomRGB = () =>
+//   `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+//     Math.random() * 256
+//   )},${Math.floor(Math.random() * 256)})`;
+// console.log(randomRGB());
+// console.log(randomRGB());
+// console.log(randomRGB());
+// console.log(randomRGB());
+// console.log(randomRGB());
+//#endregion
+
+
+//#region 08
+// console.log("Exercice 08: ");
+// console.log("===============");
+// const occurencies = (str, letter) => Array.from(str).reduce((aggregate, current) => aggregate = current === letter ? aggregate + 1 : aggregate, 0);
+// console.log(occurencies("hello", "l"));
+// console.log(occurencies("abracadabra", "a"));
+// console.log(occurencies("oups", "z"));
+//#endregion
+
+
+//#region 09
+console.log("Exercice 09: ");
 console.log("===============");
 
 
