eda846ed29995a5ddd9d4adb8a84f8f955f00b15
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-04-30 14:16:07 +0200

    removed coding challenge folder

--- Coding-Challenge/app.js
+++
@@ -1,7 +0,0 @@
-function getMiddle(s) {
-  const sLength = s.length;
-  return sLength % 2 === 0 ? s[sLength / 2 - 1] + s[sLength / 2] : s[Math.floor(s.length / 2)];
-}
-
-console.log(getMiddle("testing"));
-console.log(getMiddle("middle"));
