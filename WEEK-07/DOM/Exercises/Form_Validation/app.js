8875e6e167b4ec21b854ab1e9cc39abe414210aa
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-05-29 11:56:53 +0200

    userName input validation done

--- WEEK-07/DOM/Exercises/Form_Validation/app.js
+++ WEEK-07/DOM/Exercises/Form_Validation/app.js
@@ -1,19 +1,27 @@
+// inputs
+const inputs = document.querySelectorAll("input");
+let userName = inputs[0];
+let email = inputs[1];
+let pswd = inputs[2];
+let confirmpswd = inputs[3];
+
+// validation messages
+const validationMessages = document.querySelectorAll(".msg");
+let userNameMessage = validationMessages[0];
+let emailMessage = validationMessages[1];
+let passwordMessage = validationMessages[2];
+let confirmpasswordMessage = validationMessages[3];
+
 const submit = document.querySelector(".btn-submit");
 const reset = document.querySelector(".btn-reset");
 submit.addEventListener("click", (e) => onSubmit(e));
-reset.addEventListener("click", (e) => onReset(e));
+reset.addEventListener("click", onReset);
 // regex email:
 // (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
 
 function onSubmit(e) {
   e.preventDefault();
-
-  // inputs
-  const inputs = document.querySelectorAll("input");
-  let userName = inputs[0];
-  let email = inputs[1];
-  let pswd = inputs[2];
-  let confirmpswd = inputs[3];
+  resetMessages();
 
   // inputs values
   const inputsValues = Array.from(inputs).map((x) => x.value);
@@ -22,38 +30,58 @@ function onSubmit(e) {
   const pswdValue = inputsValues[2];
   const confirmpswdValue = inputsValues[3];
 
-  // validation messages
-  const validationMessages = document.querySelectorAll(".msg");
-  let userNameMessage = validationMessages[0];
-  let emailMessage = validationMessages[1];
-  let passwordMessage = validationMessages[2];
-  let confirmpasswordMessage = validationMessages[3];
-  validationMessages.forEach((x) => x.classList.add("red"));
-
   let valid = true;
 
-  userName.classList.add("green");
+  valid = validateUserName(userNameValue);
+  if (valid) {
+    setGreen(userName);
+    userNameMessage.innerText = "Success!";
+    setGreen(userNameMessage);
+  }
+  if (valid) {
+    inputs.forEach((x) => console.log(x.value));
+  }
+}
+
+function validateUserName(userNameValue) {
+  let userNameValid = true;
+
   if (userNameValue !== userNameValue.toLowerCase()) {
     userNameMessage.innerText += "Name must be in lower case.";
-    userName.classList.remove("green");
-    userName.classList.add("red");
-    valid = false;
+    setRed(userNameMessage);
+    setRed(userName);
+    userNameValid = false;
   }
 
   if (userNameValue.length < 5 || userNameValue.length > 10) {
     userNameMessage.innerText += `${
       userNameMessage.innerText === "" ? "" : "\n"
     }Name must be between 5 and 10 characters.`;
-    userName.classList.remove("green");
-    userName.classList.add("red");
-    valid = false;
+    setRed(userNameMessage);
+    setRed(userName);
+    userNameValid = false;
   }
-
-  if (valid) {
-  }
-  inputs.forEach((x) => console.log(x.value));
+  return userNameValid;
 }
 
-function onReset(e) {
+function onReset() {
   document.querySelector("form").reset();
+}
+
+function resetMessages() {
+  validationMessages.forEach((x) => {
+    x.innerText = "";
+    x.classList.remove("red");
+    x.classList.remove("green");
+  });
+}
+
+function setGreen(elem) {
+  elem.classList.remove("red");
+  elem.classList.add("green");
+}
+
+function setRed(elem) {
+  elem.classList.remove("green");
+  elem.classList.add("red");
 }
