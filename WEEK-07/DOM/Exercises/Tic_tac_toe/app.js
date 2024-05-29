0301ed78351e26e0af4f2b96bd3078dbd231a217
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-05-29 17:39:57 +0200

    commit to push the repo and pull on my other computer

--- WEEK-07/DOM/Exercises/Tic_tac_toe/app.js
+++ WEEK-07/DOM/Exercises/Tic_tac_toe/app.js
@@ -1,11 +1,20 @@
 const board = document.querySelector(".board");
+let turn = 1;
 
-for (let i = 1; i <= 9; i++) {
+for (let i = 0; i < 9; i++) {
   const elem = document.createElement("div");
   elem.classList.add("cell");
+  elem.setAttribute("id", i);
+  elem.addEventListener("click", (e) => {
+    // Si la cellule n'est pas vide : alert("Someone clicked this cell already.");
+    // Si la cellule est vide : mettre un X ou un O en fonction du tour.
+  });
   board.appendChild(elem);
 }
 
+const cells = document.querySelectorAll(".cell");
+const player1 = "X";
+const player2 = "O";
 const winCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -16,3 +25,32 @@ const winCombinations = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+const playedCells = ["", "", "", "", "", "", "", "", ""];
+
+// function gameStart() {
+//   board.addEventListener("click", (e) => {
+//     if (turn === 9) {
+//       gameOver();
+//     }
+//     if (e.target.classList.contains("cell")) {
+//       if (e.target.innerText === "") {
+//         e.target.innerText = turn % 2 === 0 ? player2 : player1;
+//         console.log(e.target);
+//         turn++;
+//       } else {
+//         alert("Someone clicked this cell already");
+//       }
+//     }
+//   });
+// }
+
+function gameStart() {
+    
+}
+
+function gameOver() {
+  console.log("Game over");
+}
+
+gameStart();
