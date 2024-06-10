adadef3dca4719a21b49e6def5aa8036bcd51089
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-06-10 16:39:26 +0200

    AOS exercise done

--- WEEK-09/06.AsyncAwait/app.js
+++ WEEK-09/06.AsyncAwait/app.js
@@ -2,7 +2,7 @@ function generateBg(color, delay) {
   const rand = Math.random();
   return new Promise((resolve, reject) => {
     if (rand > 0.7) {
-      reject();
+      reject("oh no! an error");
     } else {
       setTimeout(() => {
         document.body.style.backgroundColor = color;
@@ -27,7 +27,7 @@ const callGenerateBg = async () => {
       await generateBg(color, 1000);
     }
   } catch (ex) {
-    console.log("oh no! an error");
+    console.log(ex);
   }
 };
 
