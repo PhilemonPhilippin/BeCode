88e65c3f14abd925ffd9ce76702e2b2b94ec2346
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-06-11 12:02:21 +0200

    grid first version done

--- WEEK-09/16.Final-exercise/app.js
+++ WEEK-09/16.Final-exercise/app.js
@@ -1,35 +1,37 @@
 const searchButton = document.querySelector(".btn-search");
+const grid = document.querySelector(".grid");
+const searchResultText = document.querySelector(".search-result-text");
 
 searchButton.addEventListener("click", searchMeals);
 
 async function searchMeals() {
-  try {
-    const uri =
-      "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
-    let response = await fetch(uri);
-    console.log(response);
-    let data = await response.json();
-    console.log(data);
-  } catch (ex) {
-    console.log(ex);
-  }
+  const input = document.querySelector("#search");
+  const searchString = input.value;
+  const response = await getMeals(searchString);
+  updateGrid(response);
+  updateSearchResultText(searchString);
 }
 
-// async function searchMeals() {
-//   const input = document.querySelector("#search");
-//   const searchString = input.value;
-//   const response = await getMeals(searchString);
-//   console.log(response);
-// }
+function updateSearchResultText(text) {
+  searchResultText.innerText = `These are the results for "${text}" :`;
+}
 
-// async function getMeals(searchString) {
-//   const uri = `www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`;
-//   const response = await fetch(uri, {
-//     method: "GET",
-//     headers: {
-//       "api-key": "1",
-//     },
-//   });
-//   const meals = await response.json();
-//   console.log(meals);
-// }
+function updateGrid(meals) {
+  meals.forEach((meal, index) => {
+    const thumb = meal["strMealThumb"];
+    let imageElem = document.createElement("img");
+    imageElem.src = thumb;
+    const div = document.createElement("div");
+    div.classList.add(`grid-item-${index}`);
+    div.classList.add("grid-item");
+    div.appendChild(imageElem);
+    grid.appendChild(div);
+  });
+}
+
+async function getMeals(searchString) {
+  const uri = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`;
+  const response = await fetch(uri);
+  const json = await response.json();
+  return json["meals"];
+}
