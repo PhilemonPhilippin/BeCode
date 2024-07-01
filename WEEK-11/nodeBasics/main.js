bde1f4b0f1d37715b621efb7f6b880ae927a063b
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-07-01 15:20:30 +0200

    filesystem module done

--- WEEK-11/nodeBasics/main.js
+++ WEEK-11/nodeBasics/main.js
@@ -1,6 +1,7 @@
 const Circle = require("./part.js");
 // const partJs = require("./part.js");
-const os = require("node:os"); 
+const os = require("node:os");
+const fs = require("node:fs");
 
 const circle = new Circle(5);
 console.log(circle.area);
@@ -13,3 +14,41 @@ const platform = os.platform();
 const homedir = os.homedir();
 console.log(platform);
 console.log(homedir);
+
+const newFolderName = "./test";
+try {
+  if (!fs.existsSync(newFolderName)) {
+    fs.mkdirSync(newFolderName);
+  }
+} catch (err) {
+  console.error(err);
+}
+
+const content = "I am a BeCode warrior";
+const filePath = newFolderName + "/notes.txt";
+try {
+  fs.writeFileSync(filePath, content);
+} catch (err) {
+  console.error(err);
+}
+
+try {
+  const notes = fs.readFileSync(filePath, "utf8");
+  console.log(notes);
+} catch (err) {
+  console.error(err);
+}
+
+try {
+  fs.unlinkSync(filePath);
+  console.log("File removed successfully");
+} catch (err) {
+  console.error(err);
+}
+
+try {
+  fs.rmdirSync(newFolderName);
+  console.log("Folder deleted successfully");
+} catch (err) {
+  console.error(err);
+}
