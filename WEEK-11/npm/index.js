b2c2081c6b84100f996b00b91529404d8cab9dcc
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-07-02 15:39:32 +0200

    express done

---
+++ WEEK-11/npm/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const options = {
+  root: __dirname,
+};
+
+app.get("/", (req, res) => {
+  res.sendFile("home.html", options);
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("about.html", options);
+});
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).sendFile("404.html", options);
+});
+
+app.post("/", (req, res) => {
+  res.status(200).json({
+    school: "BeCode",
+    year: "2023",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
