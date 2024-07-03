5879cdeeab9e0ad15fbc26f862da7a4010795e12
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-07-03 10:14:00 +0200

    middlewares

---
+++ WEEK-11/middlewares/main.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+// app.use();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
