3ffebe78aa000e63780da1280990def5612f9d7b
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-07-04 16:59:58 +0200

    delete put dodne

--- WEEK-11/userApi/main.js
+++ WEEK-11/userApi/main.js
@@ -15,22 +15,25 @@ class User {
   }
 }
 
-const user1 = new User(
-  "KUGF-57JNJ-JFNJDK-OUH3",
-  "Glouglou43",
-  "Henrique",
-  "Vieira",
-  35,
-  true
-);
+const user1 = new User(uuidv4(), "Glouglou43", "Henrique", "Vieira", 35, true);
 
 let data = [user1];
 let uriBase = "/api/users";
 
 app.use(bodyParser.json());
 
+app.use(uriBase + "/:uid", (req, res, next) => {
+  const uid = req.params.uid;
+  const userFetched = data.find((user) => user.id === uid);
+  if (userFetched === undefined) {
+    res.statusCode = 404;
+    res.send("Invalid id: user not found.");
+  } else {
+    next();
+  }
+});
+
 app.get(uriBase, (req, res) => {
-  // console.log(uuidv4());
   res.send(data);
 });
 
@@ -41,8 +44,41 @@ app.post(uriBase, (req, res) => {
     res.send(req.body);
   } else {
     res.statusCode = 300;
-    res.send("Invalid user: ");
+    res.send("Invalid user: bad input format.");
   }
+});
+
+app.get(uriBase + "/:uid", (req, res) => {
+  const uid = req.params.uid;
+  const userFetched = data.find((user) => user.id === uid);
+  res.send(userFetched);
+});
+
+app.put(uriBase + "/:uid", (req, res) => {
+  const uid = req.params.uid;
+  const userPut = req.body;
+  const userFetched = data.find((user) => user.id === uid);
+  const index = data.indexOf(userFetched);
+
+  const userEdited = new User(
+    uid,
+    userPut.username,
+    userPut.firstName,
+    userPut.lastName,
+    userPut.age,
+    userPut.isAdmin
+  );
+
+  data[index] = userEdited;
+  res.sendStatus(204);
+});
+
+app.delete(uriBase + "/:uid", (req, res) => {
+  const uid = req.params.uid;
+  const userFetched = data.find((user) => user.id === uid);
+  const index = data.indexOf(userFetched);
+  data.splice(index, 1);
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {
