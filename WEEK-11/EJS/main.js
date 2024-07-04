77f23942dc9d2f627557ddc8e35b7f78c1c3f4b3
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-07-04 09:52:29 +0200

    ejs exercise done

--- WEEK-11/EJS/main.js
+++ WEEK-11/EJS/main.js
@@ -2,6 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const posts = [
+  {
+    title: "Post one",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, tempora! Voluptatibus, quod.",
+    author: "King Philippe",
+  },
+  {
+    title: "Post two",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, tempora! Voluptatibus, quod.",
+    author: "Barack Obama",
+  },
+  {
+    title: "Post three",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente, tempora! Voluptatibus, quod.",
+    author: "Nelson Mandela",
+  },
+];
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -9,7 +30,7 @@ app.get("/", (req, res) => {
   const hour = date.getHours();
   const minute = date.getMinutes();
   const time = `${hour}h${minute}`;
-  res.render("index", { time: time });
+  res.render("index", { time: time, posts: posts });
 });
 
 app.listen(port, () => {
