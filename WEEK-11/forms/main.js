c5c2bbd558dc196e8687b4222eaf6899973f6f36
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-07-04 11:58:13 +0200

    forms done

--- WEEK-11/forms/main.js
+++ WEEK-11/forms/main.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
@@ -6,21 +7,35 @@ app.set("view engine", "ejs");
 
 const partialPathRoot = "./partials/";
 
-let data = {
+let data = [];
+
+let renderData = {
   title: "",
   partialPath: partialPathRoot,
+  users: data,
 };
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  data.title = "Home";
-  data.partialPath = partialPathRoot + "home";
-  res.render("index", data);
+  renderData.title = "Home";
+  renderData.partialPath = partialPathRoot + "home";
+  renderData.users = data;
+  res.render("index", renderData);
 });
 
 app.get("/add", (req, res) => {
-  data.title = "Add";
-  data.partialPath = partialPathRoot + "add";
-  res.render("index", data);
+  renderData.title = "Add";
+  renderData.partialPath = partialPathRoot + "add";
+  res.render("index", renderData);
+});
+
+app.post("/add", (req, res) => {
+  data.push(req.body);
+  renderData.users = data;
+  console.log(data);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
