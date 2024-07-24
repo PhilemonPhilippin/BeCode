aa385a2a4ac30665b77a44beb3454a22152c5546
Philémon Philippin <91825951+PhilemonPhilippin@users.noreply.github.com>
2024-07-24 16:25:35 +0200

    cookies

--- WEEK-14/NodeAuthTutorial/node-express-jwt-auth-lesson-1/app.js
+++ WEEK-14/NodeAuthTutorial/node-express-jwt-auth-lesson-1/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -32,3 +34,17 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 
 app.use(authRoutes);
+
+app.get("/set-cookies", (req, res) => {
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+  res.send("you got the cookies.");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  res.json(cookies);
+});
